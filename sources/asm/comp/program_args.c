beadb7e74636067c6eded889fac8471c348c592c
Roman Gonchar <goncharoman@gmail.com>
2020-05-01 23:08:23 +0300

    fix: use instrct beign position

--- sources/asm/comp/program_args.c
+++ sources/asm/comp/program_args.c
@@ -6,34 +6,36 @@
 /*   By: ujyzene <ujyzene@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/23 22:09:07 by ujyzene           #+#    #+#             */
-/*   Updated: 2020/05/01 00:04:54 by ujyzene          ###   ########.fr       */
+/*   Updated: 2020/05/01 18:43:48 by ujyzene          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <asm_comp.h>
 
-void	write_call(t_op *op, t_token *token, t_program *program)
+void	proc_call(t_op *op, t_token *token, t_program *program,
+			int32_t instrct_position)
 {
-	uint8_t	size;
+	uint8_t		size;
 	t_label		*label;
 
 	size = token->type == DIRL ? op->tdir_size : IND_SIZE;
 	if (!(label = find_label(&program->labels , token->value)))
 		add_label(&program->labels, (label = create_label(token->value, -1)));
-	add_call(&label->calls, create_call(token, program->position, size));
+	add_call(&label->calls,
+		create_call(token, program->position, instrct_position, size));
 	program->position += size;
 }
 
-void	write_num(t_op *op, t_token *token, t_program *program)
+void	proc_num(t_op *op, t_token *token, t_program *program)
 {
 	uint8_t	size;
 
 	size = (token->type == DIR) ? op->tdir_size : IND_SIZE;
-	write_int(program, size,
+	write_num(program, size,
 		size == 2 ? (int16_t)ft_atoi32(token->value) : ft_atoi32(token->value));
 }
 
-void	write_reg(t_token *token, t_program *program)
+void	proc_reg(t_token *token, t_program *program)
 {
-	write_int(program, 1, (int8_t)ft_atoi32(&token->value[1]));
+	write_num(program, 1, (int8_t)ft_atoi32(&token->value[1]));
 }
