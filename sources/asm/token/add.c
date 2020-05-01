649d63f7e10369f8edea309b93ebc75f976fdf7e
Roman Gonchar <goncharoman@gmail.com>
2020-05-01 23:02:02 +0300

    fix: use ft_lst instead of ft_lstnew

--- sources/asm/token/add.c
+++ sources/asm/token/add.c
@@ -6,7 +6,7 @@
 /*   By: ujyzene <ujyzene@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/17 21:02:42 by ujyzene           #+#    #+#             */
-/*   Updated: 2020/04/23 12:20:55 by ujyzene          ###   ########.fr       */
+/*   Updated: 2020/05/01 22:56:00 by ujyzene          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	add_token(t_list **tokens, t_token *new_token)
 {
 	t_list	*tmp;
 
-	if (!(tmp = ft_lstnew(new_token, sizeof(t_token))))
+	if (!(tmp = ft_lst(new_token, sizeof(t_token))))
 		exit(-1);
 	ft_lstappend(tokens, tmp);
 }
@@ -31,7 +31,7 @@ void	add_endtoken(t_list **tokens, t_type type, unsigned row, unsigned col)
 		if (((token = tail->content)->type == ENDLN && type == END) ||
 			(token->type != ENDLN && type == ENDLN))
 		{
-			if (!(tail->next = ft_lstnew(create_token(NULL, type, row, col),
+			if (!(tail->next = ft_lst(create_token(NULL, type, row, col),
 				sizeof(t_token))))
 			if (!(tail->next))
 				exit(1);
