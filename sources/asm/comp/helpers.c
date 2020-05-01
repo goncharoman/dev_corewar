4183b5034095a03c535dd7d4b08c33ef5008bf3c
Roman Gonchar <goncharoman@gmail.com>
2020-05-01 23:08:52 +0300

    some changes

--- sources/asm/comp/helpers.c
+++ sources/asm/comp/helpers.c
@@ -6,25 +6,32 @@
 /*   By: ujyzene <ujyzene@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/22 15:40:43 by ujyzene           #+#    #+#             */
-/*   Updated: 2020/05/01 00:05:26 by ujyzene          ###   ########.fr       */
+/*   Updated: 2020/05/01 18:42:01 by ujyzene          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <asm_comp.h>
 
-t_bool	is_cmd(char *arg, const char *command)
+t_bool		is_cmd(char *arg, const char *command)
 {
 	if (!arg || !command)
 		return (false);
 	return (!ft_strcmp(arg, command + 1));
 }
 
-void	write_int(t_program *program, uint8_t size, int32_t value)
+uint32_t	write_int(uint8_t *code, uint32_t position, uint8_t size,
+			int32_t value)
 {
 	uint8_t	i;
 
 	i = 0;
 	while (i < size)
-		program->code[program->position++] = \
-			(uint8_t)((value >> (8 * (size - ++i))) & 0xff);
+		code[position++] = (uint8_t)((value >> (8 * (size - ++i)) & 0xFF));
+	return ((uint32_t)size);
+}
+
+void		write_num(t_program *program, uint8_t size, int32_t value)
+{
+	program->position += \
+		write_int(program->code, program->position, size, value);
 }
