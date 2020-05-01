b84eaa1b5b6a3780c09d08c47748cf3e484f0c9f
Roman Gonchar <goncharoman@gmail.com>
2020-05-01 23:13:03 +0300

    add del_call for free

--- sources/asm/comp/call.c
+++ sources/asm/comp/call.c
@@ -6,13 +6,14 @@
 /*   By: ujyzene <ujyzene@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/29 20:49:56 by ujyzene           #+#    #+#             */
-/*   Updated: 2020/04/30 00:29:29 by ujyzene          ###   ########.fr       */
+/*   Updated: 2020/05/01 19:32:03 by ujyzene          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <asm_comp.h>
 
-t_call	*create_call(t_token *token, unsigned position, unsigned size)
+t_call	*create_call(t_token *token, int32_t position,
+			int32_t instrct_position, uint32_t size)
 {
 	t_call	*call;
 
@@ -21,6 +22,7 @@ t_call	*create_call(t_token *token, unsigned position, unsigned size)
 	call->row = token->row;
 	call->col = token->col - (token->type == DIRL ? 1 : 2);
 	call->position = position;
+	call->instrct_position = instrct_position;
 	call->size = size;
 	return (call);
 }
@@ -28,4 +30,11 @@ t_call	*create_call(t_token *token, unsigned position, unsigned size)
 void	add_call(t_list **calls, t_call *call)
 {
 	ft_lstappend(calls, ft_lst(call, sizeof(t_call)));
+}
+
+void	del_call(void *call, size_t size)
+{
+	if (call)
+		free(call);
+	size = 0;
 }
