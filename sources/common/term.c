53825b5590e36c69491401aa72e65938311a6470
Roman Gonchar <goncharoman@gmail.com>
2020-06-11 23:29:06 +0300

    norm fix

--- sources/common/term.c
+++ sources/common/term.c
@@ -6,13 +6,13 @@
 /*   By: ujyzene <ujyzene@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/04 18:20:35 by ujyzene           #+#    #+#             */
-/*   Updated: 2020/05/15 18:37:35 by ujyzene          ###   ########.fr       */
+/*   Updated: 2020/06/11 23:26:52 by ujyzene          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <common.h>
 
-void	term(const char *message )
+void	term(const char *message)
 {
 	if (errno == 0)
 		ft_fprintf(2, "%s\n", message);
