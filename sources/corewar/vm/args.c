81ec57f08748a556c143be9328cbbf37ff492c8a
Roman Gonchar <goncharoman@gmail.com>
2020-06-17 16:41:46 +0300

    fix with types

--- sources/corewar/vm/args.c
+++ sources/corewar/vm/args.c
@@ -6,7 +6,7 @@
 /*   By: ujyzene <ujyzene@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/11 05:35:27 by ujyzene           #+#    #+#             */
-/*   Updated: 2020/06/15 19:03:14 by ujyzene          ###   ########.fr       */
+/*   Updated: 2020/06/17 05:26:18 by ujyzene          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,12 @@ int32_t	get_avalue(uint8_t *mem, t_cursor *cursor, uint32_t *offset,
 	int32_t	value;
 
 	value = 0;
-	if (cursor->oper_args_types[index] == REG_CODE)
+	if (cursor->oper_args_types[index] == T_REG)
 		value = cursor->reg[get_byte(mem, cursor->pc + *offset) - 1];
-	else if (cursor->oper_args_types[index] == DIR_CODE)
+	else if (cursor->oper_args_types[index] == T_DIR)
 		value = get_value(mem, cursor->pc + *offset,
 			cursor->oper->tdir_size);
-	else if (cursor->oper_args_types[index] == IND_CODE)
+	else if (cursor->oper_args_types[index] == T_IND)
 		value = get_value(mem,
 			cursor->pc + (get_addr(mem, cursor->pc + *offset) % IDX_MOD),
 			DIR_SIZE);
